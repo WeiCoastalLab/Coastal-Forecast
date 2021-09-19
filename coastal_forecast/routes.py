cefefa696187c38a58f8523bfb9fb74b3ce770af
amdavison <andrewmdavison60@gmail.com>
2021-09-19 17:50:52 -0400

    Loaded map image and updated routes and route html files and main site layout.

--- coastal_forecast/routes.py
+++ coastal_forecast/routes.py
@@ -4,5 +4,21 @@ from coastal_forecast import app
 
 # home page
 @app.route("/")
+@app.route("/41013")
 def home():
-    return render_template('layout.html')
+    return render_template('41013.html', title='41013')
+
+
+@app.route("/41009")
+def canaveral():
+    return render_template('41009.html', title='41009')
+
+
+@app.route("/44013")
+def boston():
+    return render_template('44013.html', title='44013')
+
+
+@app.route("/41008")
+def grays():
+    return render_template('41008.html', title='41008')
