e5653a067b0bda1f3e70f38e874b0d2aeafa20ee
amdavison <andrewmdavison60@gmail.com>
2021-11-28 13:25:50 -0500

    added scheduler instance and commented out debug run for app

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from coastal_forecast import app
+from coastal_forecast import app, scheduler
+from coastal_forecast.routes import scheduled_task
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    scheduler.add_job(scheduled_task, 'interval', seconds=5)
+    scheduler.start()
+    app.run()  #debug=True)
