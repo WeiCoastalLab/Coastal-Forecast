9d14003218f364f048b38eb0eb589b77642c1b91
amdavison <andrewmdavison60@gmail.com>
2021-11-28 13:24:35 -0500

    added apscheduler

--- coastal_forecast/__init__.py
+++ coastal_forecast/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
+scheduler = BackgroundScheduler()
 
 from coastal_forecast import routes  # noqa E402
