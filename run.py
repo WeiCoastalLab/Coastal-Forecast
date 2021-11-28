f758a34d76e08d564c66603c95ef5bf4eaa44622
amdavison <andrewmdavison60@gmail.com>
2021-11-28 18:19:04 -0500

    set scheduler to every minute

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from coastal_forecast import app, scheduler
 from coastal_forecast.routes import scheduled_task
 
 if __name__ == "__main__":
-    scheduler.add_job(scheduled_task, 'interval', seconds=5)
+    scheduled_task()
+    scheduler.add_job(scheduled_task, 'interval', minutes=1)
     scheduler.start()
     app.run()  #debug=True)
