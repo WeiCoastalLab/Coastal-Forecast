0591aae446fb87ca37bd8af8d26797f78ead68a6
amdavison <andrewmdavison60@gmail.com>
2021-12-14 12:15:23 -0500

    added selenium testing for chrome browser.

---
+++ tests/system/test_system_chrome.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+from selenium import webdriver
+import unittest
+
+
+class TestAppChrome(unittest.TestCase):
+    driver = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.driver = webdriver.Chrome(executable_path='../drivers/chromedriver.exe')
+        cls.driver.implicitly_wait(10)
+        cls.driver.maximize_window()
+
+    def test_app_chrome(self):
+        self.driver.get('http://127.0.0.1:5000/')
+        sleep(5)
+        self.driver.get(self.driver.find_element_by_id('canaveral').get_attribute("href"))
+        sleep(5)
+        self.driver.get(self.driver.find_element_by_id('grays_reef').get_attribute("href"))
+        sleep(5)
+        self.driver.get(self.driver.find_element_by_id('frying_pan').get_attribute("href"))
+        sleep(5)
+        self.driver.get(self.driver.find_element_by_id('boston').get_attribute("href"))
+        sleep(5)
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.driver.close()
+        cls.driver.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
