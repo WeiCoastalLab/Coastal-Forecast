54c1bd1f08b7046618f41ccb7bc0f7ed8fd559da
amdavison <andrewmdavison60@gmail.com>
2021-12-16 12:28:54 -0500

    updated options

--- tests/system/test_system_chrome.py
+++ tests/system/test_system_chrome.py
@@ -6,8 +6,9 @@ import unittest
 
 class TestAppChrome(unittest.TestCase):
     options = webdriver.ChromeOptions()
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--headless')
     options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
     @classmethod
