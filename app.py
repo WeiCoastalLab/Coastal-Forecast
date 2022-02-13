276c0ce649d132dac46d73dfc96a1d96cf7e9e7f
amdavison <andrewmdavison60@gmail.com>
2022-02-13 10:30:16 -0500

    updated app.py for use of clock.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from coastal_forecast.routes import run_predictions
 
 if __name__ == "__main__":
     run_predictions()
-    scheduler.add_job(run_predictions, 'interval', hours=6)
-    scheduler.start()
     app.run(debug=True, use_reloader=False)
+    # scheduler.add_job(run_predictions, 'interval', hours=6)
+    # scheduler.start()
+    # app.run(debug=True, use_reloader=False)
