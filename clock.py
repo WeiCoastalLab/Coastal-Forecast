e8c71e927bf68a38c7dfa9ddfc7f89ba6069e5f5
amdavison <andrewmdavison60@gmail.com>
2022-02-14 09:19:04 -0500

    updated clock.py to handle warnings

--- clock.py
+++ clock.py
@@ -1,7 +1,8 @@
+import pytz
 from apscheduler.schedulers.blocking import BlockingScheduler
 from coastal_forecast.routes import run_predictions
 
-sched = BlockingScheduler()
+sched = BlockingScheduler(timezone=pytz.UTC)
 
 
 @sched.scheduled_job('interval', hours=6)
