fdb6e2877c3516df483ba87dfecdbfd370019958
amdavison <andrewmdavison60@gmail.com>
2022-03-02 13:56:57 -0500

    refactored, removing excess code

--- coastal_forecast/__init__.py
+++ coastal_forecast/__init__.py
@@ -1,8 +1,6 @@
 # Created by Andrew Davison
 from flask import Flask
-from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
-scheduler = BackgroundScheduler()
 
 from coastal_forecast import routes  # noqa E402
