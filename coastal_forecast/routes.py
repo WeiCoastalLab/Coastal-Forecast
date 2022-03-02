cce38c26c291943d286209e74d56353895a531ba
amdavison <andrewmdavison60@gmail.com>
2022-03-02 13:54:16 -0500

    refactored for getting metadata of plot image and update params, deleted run_puredictions

--- coastal_forecast/routes.py
+++ coastal_forecast/routes.py
@@ -1,23 +1,22 @@
 # Created by Andrew Davison
 from flask import render_template, url_for
 from datetime import datetime, timedelta
-from coastal_forecast import app, prediction_manager as pm
-
-stations = ['41008', '41009', '41013', '44013']
-pred_time = dict.fromkeys(stations, datetime.utcnow().strftime('%m/%d/%Y %H:%M'))
+from coastal_forecast import app
+from PIL import Image
 
 
-def process_params(station_id: str) -> tuple:
+def process_params(station_id: str) -> tuple[str, str, str]:
     """
     Processes and returns current prediction time, next prediction time,
     and plot image location for rendering of template
     :param station_id: identification number of station to process
     :return: tuple of current prediction time, next prediction time, and relative plot image path
     """
-    current_time = pred_time[station_id]
-    next_time = (datetime.strptime(pred_time[station_id],
-                                   '%m/%d/%Y %H:%M') + timedelta(hours=6)).strftime('%m/%d/%Y %H:%M')
     plot_image = url_for('static', filename=station_id + '_system_prediction.png')
+    fig = Image.open('coastal_forecast/' + plot_image)
+    fig.load()
+    current_time = fig.info['Time']
+    next_time = (datetime.strptime(current_time, '%m/%d/%Y %H:%M') + timedelta(hours=6)).strftime('%m/%d/%Y %H:%M')
     return current_time, next_time, plot_image
 
 
@@ -49,9 +48,3 @@ def grays():
     title = '41008'
     current_time, next_time, plot_image = process_params(title)
     return render_template('41008.html', title=title, current=current_time, next=next_time, plot_image=plot_image)
-
-
-def run_predictions() -> None:
-    for station in stations:
-        pm.get_prediction(station, 9, 3)
-        pred_time[station] = datetime.utcnow().strftime('%m/%d/%Y %H:%M')
