52b85faeef636f6384c701cdcbfbe978b9521d4d
amdavison <andrewmdavison60@gmail.com>
2022-03-02 13:56:48 -0500

    refactored, removing excess code

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 # Created by Andrew Davison
-from coastal_forecast import app, scheduler
-from coastal_forecast.routes import run_predictions
+from coastal_forecast import app
 
 if __name__ == "__main__":
-    run_predictions()
-    # app.run(debug=True, use_reloader=False)
-    scheduler.add_job(run_predictions, 'interval', hours=6)
-    scheduler.start()
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True)
