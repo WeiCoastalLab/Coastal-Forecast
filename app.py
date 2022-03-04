4a374ab285220717e901e363a40b638d5c40e2d5
amdavison <andrewmdavison60@gmail.com>
2022-03-04 17:59:40 -0500

    implemented background scheduler and removed clock.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
 # Created by Andrew Davison
-from coastal_forecast import app
+from apscheduler.schedulers.background import BackgroundScheduler
+from coastal_forecast import app, prediction_manager as pm
+
+
+def timed_job():
+    stations = ['41008', '41009', '41013', '44013']
+    for station in stations:
+        pm.get_prediction(station, 9, 3)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    timed_job()
+    interval = BackgroundScheduler(timezone='UTC', daemon=True)
+    interval.add_job(timed_job, 'interval', minutes=5)
+    try:
+        interval.start()
+        print("Scheduler started...")
+    except KeyboardInterrupt or SystemExit:
+        interval.shutdown(wait=False)
+    app.run(debug=True, use_reloader=False)
