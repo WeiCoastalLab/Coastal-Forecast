284b1b660338b2e5a105524165078a100ae5c0cb
amdavison <andrewmdavison60@gmail.com>
2022-03-04 19:43:11 -0500

    moved scheduler to __init__.py

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 # Created by Andrew Davison
-from apscheduler.schedulers.background import BackgroundScheduler
-from coastal_forecast import app, prediction_manager as pm
-
-
-def timed_job():
-    stations = ['41008', '41009', '41013', '44013']
-    for station in stations:
-        pm.get_prediction(station, 9, 3)
+from coastal_forecast import app
 
 
 if __name__ == "__main__":
-    timed_job()
-    interval = BackgroundScheduler(timezone='UTC', daemon=True)
-    interval.add_job(timed_job, 'interval', hours=6)
-    try:
-        interval.start()
-        print("Scheduler started...")
-    except KeyboardInterrupt or SystemExit:
-        interval.shutdown(wait=False)
     app.run(debug=True, use_reloader=False)
--- coastal_forecast/__init__.py
+++ coastal_forecast/__init__.py
@@ -1,6 +1,22 @@
 # Created by Andrew Davison
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from coastal_forecast import prediction_manager as pm
+
+
+def timed_job():
+    stations = ['41008', '41009', '41013', '44013']
+    for station in stations:
+        pm.get_prediction(station, 9, 3)
+
 
 app = Flask(__name__)
+timed_job()
+scheduler = BackgroundScheduler(timezone='UTC', daemon=True)
+scheduler.add_job(timed_job, 'interval', hours=6)
+try:
+    scheduler.start()
+except KeyboardInterrupt or SystemExit:
+    scheduler.shutdown(wait=False)
 
 from coastal_forecast import routes  # noqa E402
