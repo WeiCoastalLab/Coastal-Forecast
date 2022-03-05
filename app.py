53e57c5b477028d6ef7749dce422425733ab1e8c
amdavison <andrewmdavison60@gmail.com>
2022-03-05 11:04:51 -0500

    moved timed_job and secheduler instance to __init__.py

--- app.py
+++ app.py
@@ -1,21 +1,6 @@
 # Created by Andrew Davison
-from apscheduler.schedulers.background import BackgroundScheduler
-from coastal_forecast import app, prediction_manager as pm
-
-
-def timed_job():
-    stations = ['41008', '41009', '41013', '44013']
-    for station in stations:
-        pm.get_prediction(station, 9, 3)
+from coastal_forecast import app
 
 
 if __name__ == '__main__':
-    timed_job()
-    scheduler = BackgroundScheduler(timezone='UTC', daemon=True)
-    scheduler.add_job(timed_job, 'interval', hours=6)
-    try:
-        print("Starting scheduler...")
-        scheduler.start()
-    except KeyboardInterrupt or SystemExit:
-        scheduler.shutdown(wait=False)
     app.run(debug=True, use_reloader=False)
--- coastal_forecast/__init__.py
+++ coastal_forecast/__init__.py
@@ -1,8 +1,24 @@
 # Created by Andrew Davison
 from flask import Flask
 from whitenoise import WhiteNoise
+from apscheduler.schedulers.background import BackgroundScheduler
+from coastal_forecast import prediction_manager as pm
 
+
+def timed_job():
+    stations = ['41008', '41009', '41013', '44013']
+    for station in stations:
+        pm.get_prediction(station, 9, 3)
+
+
+timed_job()
+scheduler = BackgroundScheduler(timezone='UTC', daemon=True)
 app = Flask(__name__)
 app.wsgi_app = WhiteNoise(app.wsgi_app, root='coastal_forecast/static/')
+try:
+    print("Starting scheduler...")
+    scheduler.start()
+except KeyboardInterrupt or SystemExit:
+    scheduler.shutdown(wait=False)
 
 from coastal_forecast import routes  # noqa E402
