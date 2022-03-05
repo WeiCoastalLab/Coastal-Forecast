7a994e5d0ed3666b271093265ff3632ab8b700ec
amdavison <andrewmdavison60@gmail.com>
2022-03-05 10:38:48 -0500

    added WhiteNoise for serving static files

--- coastal_forecast/__init__.py
+++ coastal_forecast/__init__.py
@@ -1,7 +1,8 @@
 # Created by Andrew Davison
 from flask import Flask
-
+from whitenoise import WhiteNoise
 
 app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='coastal_forecast/static/')
 
 from coastal_forecast import routes  # noqa E402
