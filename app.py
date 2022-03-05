6f434467d10a21c6faeb13876257b4a5fc977985
amdavison <andrewmdavison60@gmail.com>
2022-03-04 20:43:08 -0500

    refactored

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 # Created by Andrew Davison
-from coastal_forecast import app
+from apscheduler.schedulers.background import BackgroundScheduler
+from coastal_forecast import app, prediction_manager as pm
 
 
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+def timed_job():
+    stations = ['41008', '41009', '41013', '44013']
+    for station in stations:
+        pm.get_prediction(station, 9, 3)
+
+
+timed_job()
+scheduler = BackgroundScheduler(timezone='UTC', daemon=True)
+scheduler.add_job(timed_job, 'interval', hours=6)
+try:
+    print("starting scheduler...")
+    scheduler.start()
+except KeyboardInterrupt or SystemExit:
+    scheduler.shutdown(wait=False)
+app.run(debug=True, use_reloader=False)
